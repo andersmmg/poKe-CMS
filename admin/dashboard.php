323fab3720c761dff2bf0aa8959544ba187d6340
Joshua Anderson <jazinc.company@gmail.com>
2016-11-03 10:37:08 -0600

    Fix grammatical errors
    Also removed unnecessary ending tags.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,16 +4,16 @@ include '../config.php';
 include 'functions/header.php';
 ?>
 <ol class="breadcrumb">
-  <li><div>Home</a></li>
+  <li><div>Home</div></li>
   <li class="active">Dashboard</li>
 </ol>
 <h1 class="page-header">Dashboard</h1>
 
 <div class="jumbotron">
-  <h1 class="display-3">Welcome Back, <?php echo $_SESSION['username']; ?></h1>
-  <p class="lead">This is our brand new control panel designed using bootstrap 4.</p>
+  <h1 class="display-3">Welcome back, <?php echo $_SESSION['username']; ?></h1>
+  <p class="lead">This is our brand new control panel designed using BootStrap 4.</p>
   <hr class="m-y-2">
-  <p>It is still in development so do not expect much. This control panel will be a lot better than the old multisite control panel.</p>
+  <p>It is still in development so do not expect much. This control panel will be much better than the old multisite control panel.</p>
   <p class="lead">
    
   </p>
@@ -80,13 +80,13 @@ if (file_exists('../install')) {
 <h1>0</h1>Items
 </div>
 
-</style><div id="tile"  class="col-sm-6 card card-block">
+<div id="tile"  class="col-sm-6 card card-block">
 <h1>0</h1>Pages
 </div>
 <div id="tile"  class="col-sm-6 card card-block">
 <h1>0</h1>Posts
 </div>
-</style><div id="tile"  class="col-sm-6 card card-block">
+<div id="tile"  class="col-sm-6 card card-block">
 <h1><?php
     $count = 0;
 $con = mysqli_connect($hostname, $username, $password, $db_name);
@@ -105,7 +105,7 @@ $result = mysqli_query($con, $sql);
   <div class="col-sm-6 alert alert-dismissible fade in" role="alert">
     <div class="card card-block">
       <h3 class="card-title">Add a new Post</h3>
-      <p class="card-text">When creating a website you always need a blog.</p>
+      <p class="card-text">When creating a website, you always need a blog.</p>
       <a href="post.php" class="btn btn-primary"><i class="fa fa-arrow-right fa-fw"></i>Visit Blog <span class="label label-success">Recommended</span></a>
     <a class="btn btn-danger" data-dismiss="alert" aria-label="Close">Dismiss</a>
     </div>
@@ -113,8 +113,8 @@ $result = mysqli_query($con, $sql);
   <div class="col-sm-6 alert alert-dismissible fade in" role="alert">
     
     <div class="card card-block">
-      <h3 class="card-title">Add a new Page </h3>
-      <p class="card-text">Pages make your website a website with rich content.</p>
+      <h3 class="card-title">Add new Page </h3>
+      <p class="card-text">Pages create rich content for a beautiful website.</p>
       <a href="pages.php" class="btn btn-primary"><i class="fa fa-arrow-right fa-fw"></i>Visit Page Creator <span class="label label-success">Recommended</span></a>
       <a class="btn btn-danger" data-dismiss="alert" aria-label="Close">Dismiss</a>
     </div>
